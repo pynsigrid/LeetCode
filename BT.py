da0566a537b70f1d38702d1d0555501f7dde8bf7
pynsigrid <panyn_sigrid@163.com>
2021-08-23 17:13:23 +0800

    Review 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先

--- BT.py
+++ BT.py
@@ -1,48 +1,40 @@
 from typing import List
-class BinaryTree():
-    def __init__(self, val, left, right):
-        self.val = val
-        self.left = left
-        self.right = right
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-bt1 = BinaryTree(1, None, None)
-bt2 = BinaryTree(2, None, None)
-bt3 = BinaryTree(3, None, None)
-bt4 = BinaryTree(4, None, None)
-bt5 = BinaryTree(5, None, None)
-bt6 = BinaryTree(3, None, None)
-bt7 = BinaryTree(7, None, None)
-bt8 = BinaryTree(8, None, None)
+t1 = TreeNode(6)
+t2 = TreeNode(2)
+t3 = TreeNode(8)
+t4 = TreeNode(0)
+t5 = TreeNode(4)
+t6 = TreeNode(7)
+t7 = TreeNode(9)
+t8 = TreeNode(3)
+t9 = TreeNode(5)
 
-bt1.left, bt1.right = bt2, bt3
-bt2.left = bt4
-bt3.left = bt6
+t1.left, t1.right = t2, t3
+t2.left, t2.right = t4, t5
+t3.left, t3.right = t6, t7
+t5.left, t5.right = t8, t9
 
 # bt6.left = bt8
 
 class Solution:
-    def pathSum(self, root: BinaryTree, target: int) -> List[List[int]]:
-        def dfs(node, target):
-            if not node.left and not node.right:
-                if target == 0:
-                    res.append(tmp.copy())
-                return res
-            if target < 0: return
-            if node.left:
-                tmp.append(node.left.val)
-                dfs(node.left, target-node.left.val)
-                tmp.pop()
-            if node.right:
-                tmp.append(node.right.val)
-                dfs(node.right, target-node.right.val)
-                tmp.pop()
-            return res
-        if not root: return []
-        res, tmp = [], []
-        tmp.append(root.val)
-        return dfs(root, target-root.val)
+    def lowestCommonAncestor(self, \
+    root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(node):
+            if not node: return
+            if node.val>p.val and node.val>q.val:
+                return dfs(node.left)  # 记录上一个返回的节点，即最近公共祖先
+            elif node.val<p.val and node.val<q.val:
+                return dfs(node.right)
+            return node  
+        return dfs(root)
 
 
 a = Solution()
-res = a.pathSum(bt1, 7)
-print(res)
+res = a.lowestCommonAncestor(t1, t8, t9)
+print(res.val)
