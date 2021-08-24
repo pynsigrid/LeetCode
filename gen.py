65a7f7de8d1a673be1cf5eba3efa175ddc367a91
pynsigrid <panyn_sigrid@163.com>
2021-08-24 17:04:55 +0800

    Review 26. 删除排序数组的重复项
    双指针

--- gen.py
+++ gen.py
@@ -1,27 +1,33 @@
 class Solution:
-    def movingCount(self, m: int, n: int, k: int) -> int:
-        def dsum(i):
-            n = 0
-            while i:
-                n += i%10
-                i //= 10
-            return n
-        def dfs(i, j, k):
-            
-            if i>=m or i<0 or j>=n or j<0 \
-                or dsum(i)+dsum(j)>k or (i,j) in used:
-                return 0
-            used.add((i, j))
-            print(used)
-            res = 1+dfs(i+1,j,k)+dfs(i-1,j,k)+dfs(i,j-1,k)+dfs(i,j+1,k)
-            # used.pop()
-            return res
-        used = set()
-        return dfs(0,0,k)
+    def strStr(self, haystack, needle):
+        H, N = len(haystack), len(needle)
+        if not N: return 0
+        
+        p1 = 0
+        while p1 < H:
+            print(p1)
+            if haystack[p1] != needle[0]:
+                p1 += 1
+            else:
+                start_match = p1
+                p2 = 0
+                while p2 < N:
+                    print(p1, p2, haystack[start_match:p2+1])
+                    if haystack[p1] != needle[p2]:
+                        p1 = start_match+1
+                        break
+                    elif p2-start_match+1 == N:
+                        return start_match
+                    else:
+                        p1 += 1
+                        p2 += 1
+        return -1
 
 board = [["C","A","A"],["A","A","A"],["B","C","D"]]
 word = "AAB"
 
+haystack = "mississippi"
+needle = "issip"
 a = Solution()
-res = a.movingCount(3,6,5)
+res = a.strStr(haystack, needle)
 print(res)
