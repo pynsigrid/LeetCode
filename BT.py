9a69077495a2585550df64d6881bd3832ff2a650
pynsigrid <panyn_sigrid@163.com>
2021-08-30 22:42:40 +0800

    Review 剑指 Offer 66. 构建乘积数组

--- BT.py
+++ BT.py
@@ -15,36 +15,36 @@ t7 = TreeNode(9)
 t8 = TreeNode(3)
 t9 = TreeNode(5)
 
+
 t1.left, t1.right = t2, t3
 t2.left, t2.right = t4, t5
 t3.left, t3.right = t6, t7
 t5.left, t5.right = t8, t9
 
 # bt6.left = bt8
+class Solution:
+    def permutation(self, s: str) -> List[str]:
+        def backtrack(tmp, usd):
+            if len(usd)==len(s): 
+                res.append(''.join(tmp))
+                return
+            for i in s:
+                if i in usd: continue
+                tmp.append(i)
+                usd.append(i)
+                backtrack(tmp, usd)
+                tmp.pop()
+                usd.pop()
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        def dfs(node):
-            if p.val<=node.val<=q.val or q.val<=node.val<=p.val:
-                return node
-            elif node.val>p.val and node.val>q.val:
-                res = dfs(node.left)
-                return res
-            elif node.val<p.val and node.val<q.val:
-                res = dfs(node.right)
-                return res
-            # else:
-            #     return False
-            # return res
-        return dfs(root)
+        if not s: return []
+        res, tmp, usd = [], [], []
+        backtrack(tmp, usd)
+        return res
+                
 
 
+s = "abc"
 a = Solution()
-res = a.lowestCommonAncestor(t1, t8, t9)
-print(res.val)
+res = a.permutation(s)
+
+print(res)
